e8e438e73ade11bc5469973add8ba4622e663832
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-02-04 13:00:27 -0700

    d

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+
+
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyD9bILGUFkMuHJHdSU1oQ2D3DPZzVPbymk",
+    authDomain: "angularcam.firebaseapp.com",
+    databaseURL: "https://angularcam-default-rtdb.firebaseio.com",
+    projectId: "angularcam",
+    storageBucket: "angularcam.appspot.com",
+    messagingSenderId: "683800012934",
+    appId: "1:683800012934:web:66646383dcd40c9b770907",
+    measurementId: "G-1VL876XTKN"
+  }
+};
+
