e22656b11ce22cf3bb44f589cfe840ab25460197
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-02-04 14:23:56 -0700

    responsive camera vh

--- src/app/admin.js
+++
@@ -1,9 +0,0 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://angularcam-default-rtdb.firebaseio.com"
-});
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { WebcamModule } from 'ngx-webcam';
 import { AngularFireModule } from '@angular/fire/compat';
-import {CameraComponent} from './camera/camera.component';
-
+import { CameraComponent } from './camera/camera.component';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
