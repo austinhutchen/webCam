a51567ba6004d52a7fa8f67a1642c887c94026e4
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-02-05 10:46:59 -0700

    d

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const environment = {
+firebaseConfig : {
+},
+};
+
+// Initialize Firebase
+const app = initializeApp(environment.firebaseConfig);
