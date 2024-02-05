6edb1100dba990539feebf8db29e5f2b618e1523
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-02-05 00:53:41 -0700

    done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+export const environment = {
+ firebaseConfig : {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+}
+
+// Initialize Firebase
+const app = initializeApp(environment.firebaseConfig);
