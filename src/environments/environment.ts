765f4fac90f8e2cfec75461cae4e1602a1893f5e
Austin Hutchen <93489691+austinhutchen@users.noreply.github.com>
2024-02-06 16:09:11 -0800

    Update environment.ts
    Signed-off-by: Austin Hutchen <93489691+austinhutchen@users.noreply.github.com>

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 export const environment = {
 firebaseConfig : {
-    apiKey: "AIzaSyC_WA1YSin-B-MIl6pgqycft-upEnsjgJY",
-    authDomain: "datablobstore.firebaseapp.com",
-    databaseURL: "https://datablobstore-default-rtdb.firebaseio.com",
-    projectId: "datablobstore",
-    storageBucket: "datablobstore.appspot.com",
-    messagingSenderId: "940269214197",
-    appId: "1:940269214197:web:4bea4491c732506f4168bc"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 },
 };
 
