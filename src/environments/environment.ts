bd034568b9dead95e4a1b9af4ebd281b3247f061
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-02-07 12:33:12 -0700

    fixed env var for vercel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_apiKey,
-    authDomain: process.env.NEXT_PUBLIC_authDomain,
-    databaseURL: process.env.NEXT_PUBLIC_databaseURL,
-    projectId: process.env.NEXT_PUBLIC_projectId,
-    storageBucket: process.env.NEXT_PUBLIC_storageBucket,
-    messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
-    appId: process.env.NEXT_PUBLIC_appId
+    apiKey: process.NEXT_PUBLIC_VERCEL_ENV.NEXT_PUBLIC_apiKey,
+    authDomain: process.NEXT_PUBLIC_VERCEL_ENV.NEXT_PUBLIC_authDomain,
+    databaseURL: process.NEXT_PUBLIC_VERCEL_ENV.NEXT_PUBLIC_databaseURL,
+    projectId: process.NEXT_PUBLIC_VERCEL_ENV.NEXT_PUBLIC_projectId,
+    storageBucket: process.NEXT_PUBLIC_VERCEL_ENV.NEXT_PUBLIC_storageBucket,
+    messagingSenderId: process.NEXT_PUBLIC_VERCEL_ENV.NEXT_PUBLIC_messagingSenderId,
+    appId: process.NEXT_PUBLIC_VERCEL_ENV.NEXT_PUBLIC_appId
 };
 
 // Initialize Firebase
