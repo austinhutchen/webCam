aade94a49d9c676b88caa1a4032fcb4364f12c14
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-02-07 12:53:59 -0700

    d

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,16 +2,16 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+const secretJSON = JSON.parse( process.env.NEXT_PUBLIC_firebaseConfig)
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-    apiKey: process.NEXT_PUBLIC_VERCEL_ENV.NEXT_PUBLIC_apiKey,
-    authDomain: process.NEXT_PUBLIC_VERCEL_ENV.NEXT_PUBLIC_authDomain,
-    databaseURL: process.NEXT_PUBLIC_VERCEL_ENV.NEXT_PUBLIC_databaseURL,
-    projectId: process.NEXT_PUBLIC_VERCEL_ENV.NEXT_PUBLIC_projectId,
-    storageBucket: process.NEXT_PUBLIC_VERCEL_ENV.NEXT_PUBLIC_storageBucket,
-    messagingSenderId: process.NEXT_PUBLIC_VERCEL_ENV.NEXT_PUBLIC_messagingSenderId,
-    appId: process.NEXT_PUBLIC_VERCEL_ENV.NEXT_PUBLIC_appId
+    apiKey: secretJSON.NEXT_PUBLIC_apiKey,
+    authDomain: secretJSON.NEXT_PUBLIC_authDomain,
+    databaseURL: secretJSON.NEXT_PUBLIC_databaseURL,
+    projectId: secretJSON.NEXT_PUBLIC_projectId,
+    storageBucket: secretJSON.NEXT_PUBLIC_storageBucket,
+    messagingSenderId: secretJSON.NEXT_PUBLIC_messagingSenderId,
+    appId: secretJSON.NEXT_PUBLIC_appId
 };
 
 // Initialize Firebase
