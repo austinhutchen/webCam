e36a1055190ffb1342fd5c2c5c89081175261533
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-02-22 07:54:19 -0800

    d

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 const secretJSON = JSON.parse( process.env.NEXT_PUBLIC_firebaseConfig)
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-    apiKey: secretJSON.NEXT_PUBLIC_apiKey,
-    authDomain: secretJSON.NEXT_PUBLIC_authDomain,
-    databaseURL: secretJSON.NEXT_PUBLIC_databaseURL,
-    projectId: secretJSON.NEXT_PUBLIC_projectId,
-    storageBucket: secretJSON.NEXT_PUBLIC_storageBucket,
-    messagingSenderId: secretJSON.NEXT_PUBLIC_messagingSenderId,
-    appId: secretJSON.NEXT_PUBLIC_appId
+    apiKey: secretJSON.apiKey,
+    authDomain: secretJSON.authDomain,
+    databaseURL: secretJSON.databaseURL,
+    projectId: secretJSON.projectId,
+    storageBucket: secretJSON.storageBucket,
+    messagingSenderId: secretJSON.messagingSenderId,
+    appId: secretJSON.appId
 };
 
 // Initialize Firebase
