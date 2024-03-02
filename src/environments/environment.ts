22705fe086d2c659c9d800f9d36ab04bcc5c8581
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-03-02 11:11:44 -0800

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,17 +2,16 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-const secretJSON = JSON.parse( process.env.NEXT_PUBLIC_firebaseConfig)
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-    apiKey: secretJSON.apiKey,
-    authDomain: secretJSON.authDomain,
-    databaseURL: secretJSON.databaseURL,
-    projectId: secretJSON.projectId,
-    storageBucket: secretJSON.storageBucket,
-    messagingSenderId: secretJSON.messagingSenderId,
-    appId: secretJSON.appId
+  apiKey: "AIzaSyC_WA1YSin-B-MIl6pgqycft-upEnsjgJY",
+  authDomain: "datablobstore.firebaseapp.com",
+  databaseURL: "https://datablobstore-default-rtdb.firebaseio.com",
+  projectId: "datablobstore",
+  storageBucket: "datablobstore.appspot.com",
+  messagingSenderId: "940269214197",
+  appId: "1:940269214197:web:4bea4491c732506f4168bc",
+  measurementId: "G-FFN99SQM4M"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
