34cd5353cbdcf7116382b2e29a5b620dbe4fb634
Austin Hutchen <93489691+austinhutchen@users.noreply.github.com>
2024-06-06 07:40:15 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,7 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyC_WA1YSin-B-MIl6pgqycft-upEnsjgJY",
-  authDomain: "datablobstore.firebaseapp.com",
-  databaseURL: "https://datablobstore-default-rtdb.firebaseio.com",
-  projectId: "datablobstore",
-  storageBucket: "datablobstore.appspot.com",
-  messagingSenderId: "940269214197",
-  appId: "1:940269214197:web:4bea4491c732506f4168bc",
-  measurementId: "G-FFN99SQM4M"
+
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
